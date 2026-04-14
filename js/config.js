0cf6141ca97f54be7dfc6c06eacd891592482147
securevisions3 <icivil.jmr@gmail.com>
2026-04-14 17:06:01 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.SV_CONFIG = {
+  googleClientId: '308165480694-sfqlp29jpesoqb5h9in1dgegevdorsar.apps.googleusercontent.com',
+  allowedEmail: 'icivil.jmr@gmail.com',
+  gmailQuery: 'has:attachment',
+  pageSize: 5
+};
